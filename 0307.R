5589a38fec98d4ff5ef4b2bf4a1e0f1457f1a968
Osen Jian <osen840620@gmail.com>
2016-05-15 22:39:49 +0800

    exhw5

--- 0307.R
+++ 0307.R
@@ -8,7 +8,7 @@ c<-a+b
 x<-c(1,2,3)
 x+x
 y<-c(1,2,3,4)
-x+y
+x*y
 z<-c(1,2,3,4,5,6)
 x+z
 round(22.66666,2)
@@ -29,4 +29,11 @@ round(sd(x),2)
 summary(x)
 sort(x)
 sort(x,decreasing = T)
-
+rank(x)
+m <- 1:10
+dim(m) <- c(2, 5)
+dim(m)
+rep(1:5, times=3)
+n<-matrix(1:6, nrow = 2, ncol = 3)
+dim(n)
+vector("numeric", length = 10)
